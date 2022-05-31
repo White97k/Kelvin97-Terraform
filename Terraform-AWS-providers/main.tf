e9fd1d1c2c06e4729b37ebf52b03e849e80b68bf
KelvinLee <44488127+White97k@users.noreply.github.com>
2022-05-31 17:32:05 +0800

    Just a simple example for create an EC2-instance

--- Terraform-AWS-providers/main.tf
+++ Terraform-AWS-providers/main.tf
@@ -1,1 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"  ##edit##
+    }
+  }
+}
 
+provider "aws" {
+  profile = "default"
+  region  = "ap-southeast-1"  ##edit##
+}
+
+resource "aws_instance" "testapp_server" {   ##aws_instance = resource-type ; testapp_server = resource-name
+  ami           = "ami-xxx"  ##edit##
+  instance_type = "t2.micro"
+  vpc_security_group_ids = "sg-xxx"  ##your security-group
+  subnet_id = "subnet-xxx"  ##your subnet
+
+  tags = {
+    Name = "testapp_server"  ##your EC2-instance tags name
+  }
+}
