01dc5b1a6efac543f98744346f64f3388dbfa3cd
KelvinLee <44488127+White97k@users.noreply.github.com>
2022-06-16 16:24:00 +0800

    Create some basic network env (vpc,igw,subnet,rtb)

--- Terraform-AWS-providers/main.tf
+++ Terraform-AWS-providers/main.tf
@@ -2,23 +2,70 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"  ##edit##
+      version = "~> 4.18"
     }
   }
 }
 
 provider "aws" {
-  profile = "default"
-  region  = "ap-southeast-1"  ##edit##
+  profile = "testing"
+  region  = "ap-southeast-1"
 }
 
-resource "aws_instance" "testapp_server" {   ##aws_instance = resource-type ; testapp_server = resource-name
-  ami           = "ami-xxx"  ##edit##
-  instance_type = "t2.micro"
-  vpc_security_group_ids = "sg-xxx"  ##your security-group
-  subnet_id = "subnet-xxx"  ##your subnet
-
-  tags = {
-    Name = "testapp_server"  ##your EC2-instance tags name
+##create VPC network
+resource "aws_vpc" "mainvpc" {
+  cidr_block = "172.85.0.0/21"
+  tags       = {
+    Name = "mainvpc"
   }
 }
+
+##create IGW
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.mainvpc.id
+  tags   = {
+    Name = "mainigw"
+  }
+}
+
+##create route_table (public-network)
+resource "aws_route_table" "pub-route_table" {
+  vpc_id = aws_vpc.mainvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "pub-route_table"
+  }
+}
+
+
+##create Subnet (public-network)
+resource "aws_subnet" "pubsub1a" {
+  vpc_id     = aws_vpc.mainvpc.id
+  cidr_block = "172.85.1.0/24"
+
+  tags = {
+    Name = "Public Subnet 1A"
+  }
+}
+
+##associate subnet to rtb
+resource "aws_route_table_association" "subnettortb" {
+  subnet_id      = aws_subnet.pubsub1a.id
+  route_table_id = aws_route_table.pub-route_table.id
+}
+
+
+##create ec2-instance
+#resource "aws_instance" "app_server" {
+#  ami           = "ami-0bd6906508e74f692"
+#  instance_type = "t2.micro"
+#
+#  tags = {
+#    Name = "app-server"
+#  }
+#}
