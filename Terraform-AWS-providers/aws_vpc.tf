41f2218a71d7aefaff48aeb0e0cfccc22cd48831
white97k <xiiaob799@gmail.com>
2022-07-20 17:56:40 +0800

    updated

--- Terraform-AWS-providers/aws_vpc.tf
+++ Terraform-AWS-providers/aws_vpc.tf
@@ -1,12 +1,10 @@
 ##create VPC network
-resource "aws_vpc" "mainvpc" {
+resource "aws_vpc" "BaseNetworkVPC" {
   cidr_block = var.vpc_cidr_block
   enable_dns_hostnames = true
   enable_dns_support = true
   
   tags = {
-    Name = "mainvpc"
+    Name = "BaseNetworkVPC"
   }
 }
-
-#testtest
