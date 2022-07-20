f62d09d9a79894757b878cae803b9d95e613bfb7
KelvinLee <44488127+White97k@users.noreply.github.com>
2022-07-20 17:18:36 +0800

    Create aws_vpc.tf

---
+++ Terraform-AWS-providers/aws_vpc.tf
@@ -0,0 +1,10 @@
+##create VPC network
+resource "aws_vpc" "mainvpc" {
+  cidr_block = var.vpc_cidr_block
+  enable_dns_hostnames = true
+  enable_dns_support = true
+  
+  tags = {
+    Name = "mainvpc"
+  }
+}
