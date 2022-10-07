925cd2c3455644d5720e76e90e1a91d2e083f5c2
KelvinLee <44488127+White97k@users.noreply.github.com>
2022-10-07 15:45:08 +0800

    Update aws_autoscaling_policy.tf

--- Terraform-AWS-providers/aws_autoscaling_policy.tf
+++ Terraform-AWS-providers/aws_autoscaling_policy.tf
@@ -1,8 +1,8 @@
 resource "aws_autoscaling_policy" "CPU_Target_Tracking_Policy" {
-  count                     = "${length(var.app_type)}"
+  count                     = "${length(var.ASG-NAME)}"
   name                      = "CPU Target Tracking Policy"
   policy_type               = "TargetTrackingScaling"
-  autoscaling_group_name    = "${var.app_type[count.index]}"
+  autoscaling_group_name    = "${var.ASG-NAME[count.index]}"
   estimated_instance_warmup = 180
   target_tracking_configuration {
     predefined_metric_specification {
@@ -11,3 +11,5 @@ resource "aws_autoscaling_policy" "CPU_Target_Tracking_Policy" {
     target_value = 50.0
   }
 }
+
+### ASG-NAME = your auto scaling group name list
