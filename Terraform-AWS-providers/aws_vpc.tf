5d420b31644d7bc7d7c0133646660155f4488c35
KelvinLee <44488127+White97k@users.noreply.github.com>
2023-07-18 14:45:17 +0800

    Update aws_vpc.tf

--- Terraform-AWS-providers/aws_vpc.tf
+++ Terraform-AWS-providers/aws_vpc.tf
@@ -1,35 +1,35 @@
 # Configure the AWS Provider
 provider "aws" {
-  region = var.aws_region ##edit
+  region = var.aws_region
 }
 #Retrieve the list of AZs in the current AWS region
 data "aws_availability_zones" "available" {}
 data "aws_region" "current" {}
 #Define the VPC
 resource "aws_vpc" "vpc" {
-  cidr_block = var.vpc_cidr ##edit
+  cidr_block = var.vpc_cidr
   tags = {
-    Name        = var.vpc_name ##edit
+    Name = var.vpc_name
   }
 }
 #Deploy the private subnets
 resource "aws_subnet" "private_subnets" {
-  count = 3
-  vpc_id            = aws_vpc.vpc.id ##edit
+  count             = 3
+  vpc_id            = aws_vpc.vpc.id
   cidr_block        = cidrsubnet(var.vpc_cidr, 3, count.index)
   availability_zone = tolist(data.aws_availability_zones.available.names)[count.index]
   tags = {
-    Name      = "private_subnets${count.index + 1}"
+    Name = "private_subnets${count.index + 1}"
   }
 }
 #Deploy the public subnets
 resource "aws_subnet" "public_subnets" {
-  count = 3
-  vpc_id            = aws_vpc.vpc.id ##edit
+  count             = 3
+  vpc_id            = aws_vpc.vpc.id
   cidr_block        = cidrsubnet(var.vpc_cidr, 3, count.index + 3)
   availability_zone = tolist(data.aws_availability_zones.available.names)[count.index]
   tags = {
-    Name      = "public_subnets${count.index + 1}"
+    Name = "public_subnets${count.index + 1}"
   }
 }
 #Create route tables for public and private subnets
@@ -41,43 +41,34 @@ resource "aws_route_table" "public_route_table" {
     #nat_gateway_id = aws_nat_gateway.nat_gateway.id
   }
   tags = {
-    Name      = "demo_public_rtb"
+    Name = "demo_public_rtb"
   }
 }
 resource "aws_route_table" "private_route_tables" {
-    count = 3
+  count      = 3
   depends_on = [aws_subnet.private_subnets]
-  vpc_id     = aws_vpc.vpc.id 
+  vpc_id     = aws_vpc.vpc.id
   route {
     cidr_block = "0.0.0.0/0"
     # gateway_id = aws_internet_gateway.internet_gateway.id
     nat_gateway_id = aws_nat_gateway.nat_gateways[count.index].id
   }
   tags = {
-    Name      = "demo-private_route_tables${count.index + 1}"
+    Name = "demo-private_route_tables${count.index + 1}"
   }
 }
 
 #Create route table associations
-resource "aws_route_table_association" "public1" {
+resource "aws_route_table_association" "public" {
+  count          = 3
   depends_on     = [aws_subnet.public_subnets]
+  subnet_id      = aws_subnet.public_subnets[count.index].id
   route_table_id = aws_route_table.public_route_table.id
-  subnet_id      = aws_subnet.public_subnets[0].id
-}
-resource "aws_route_table_association" "public2" {
-  depends_on     = [aws_subnet.public_subnets]
-  route_table_id = aws_route_table.public_route_table.id
-  subnet_id      = aws_subnet.public_subnets[1].id
-}
-resource "aws_route_table_association" "public3" {
-  depends_on     = [aws_subnet.public_subnets]
-  route_table_id = aws_route_table.public_route_table.id
-  subnet_id      = aws_subnet.public_subnets[2].id
 }
 resource "aws_route_table_association" "privates" {
-  count = 3
-  depends_on = [aws_subnet.private_subnets]
-  subnet_id = aws_subnet.private_subnets[count.index].id
+  count          = 3
+  depends_on     = [aws_subnet.private_subnets]
+  subnet_id      = aws_subnet.private_subnets[count.index].id
   route_table_id = aws_route_table.private_route_tables[count.index].id
 }
 #Create Internet Gateway
@@ -89,7 +80,7 @@ resource "aws_internet_gateway" "internet_gateway" {
 }
 #Create EIP for NAT Gateway
 resource "aws_eip" "nat_gateway_eips" {
-  count = 3
+  count      = 3
   domain     = "vpc"
   depends_on = [aws_internet_gateway.internet_gateway]
   tags = {
@@ -98,10 +89,10 @@ resource "aws_eip" "nat_gateway_eips" {
 }
 #Create NAT Gateway
 resource "aws_nat_gateway" "nat_gateways" {
-  count = 3
+  count         = 3
   depends_on    = [aws_subnet.public_subnets]
   allocation_id = aws_eip.nat_gateway_eips[count.index].id
-  subnet_id = aws_subnet.public_subnets[count.index].id
+  subnet_id     = aws_subnet.public_subnets[count.index].id
   tags = {
     Name = "demo_nat_gateway${count.index + 1}"
   }
